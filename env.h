c96d27c111a4456bc6c28ad0aed9811a5bbe70db
Roy Merkel <roy_d_merkel@hotmail.com>
2022-03-20 21:32:18 -0700

    Update env.h
    Added ifdef for c++

--- env.h
+++ env.h
@@ -1,7 +1,13 @@
 #ifndef ENV_H
 #define ENV_H
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
 int setenv(const char *var_name, const char *new_value, int change_flag);
 int unsetenv(const char *name);
+#if defined(__cplusplus)
+}
+#endif
 
 #endif // #ifndef ENV_H
